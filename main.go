6df364813d229ed1e6d8d20f32f87db4b5f28d67
Toby Padilla <tobypadilla@gmail.com>
2019-01-16 16:04:28 -0600

    Custom file struct

--- main.go
+++ main.go
@@ -10,26 +10,33 @@ import (
 	"path/filepath"
 )
 
+type File struct {
+	info os.FileInfo
+	path string
+}
+
+type Files []File
+
 type Server struct {
 	port     int
 	hostname string
-	files    map[string]bool
+	files    Files
 }
 
 func NewServer(p int, h string) Server {
 	return Server{
 		port:     p,
 		hostname: h,
-		files:    make(map[string]bool),
+		files:    make([]File, 0),
 	}
 }
 
 func (me *Server) loadFiles() {
-	me.files = make(map[string]bool)
+	me.files = make([]File, 0)
 	err := filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
 		if SupportedType(path) {
 			fmt.Printf("Adding file: %s\n", path)
-			me.files[path] = true
+			me.files = append(me.files, File{info, path})
 		} else {
 			fmt.Printf("Skipping file: %s\n", path)
 		}
@@ -42,15 +49,14 @@ func (me *Server) loadFiles() {
 
 func (me *Server) m3uHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "application/mpegurl")
-	for f, _ := range me.files {
+	for _, f := range me.files {
 		fmt.Fprintf(w, "http://%s:%d/media/%s\n", me.hostname, me.port, f)
 	}
 }
 
 func (me *Server) mediaHandler(w http.ResponseWriter, r *http.Request) {
 	f := path.Base(r.URL.Path)
-	_, ok := me.files[f]
-	if ok {
+	if me.files.ContainsPath(f) {
 		http.ServeFile(w, r, f)
 	} else {
 		http.NotFound(w, r)
@@ -60,8 +66,8 @@ func (me *Server) mediaHandler(w http.ResponseWriter, r *http.Request) {
 func (me *Server) reloadHandler(w http.ResponseWriter, r *http.Request) {
 	me.loadFiles()
 	fmt.Fprintf(w, "Reloaded %d files\n", len(me.files))
-	for f, _ := range me.files {
-		fmt.Fprintf(w, "http://%s:%d/media/%s\n", me.hostname, me.port, f)
+	for _, f := range me.files {
+		fmt.Fprintf(w, "http://%s:%d/media/%s\n", me.hostname, me.port, f.path)
 	}
 }
 
@@ -72,6 +78,15 @@ func (me *Server) Start() {
 	http.HandleFunc("/media/", me.mediaHandler)
 	fmt.Printf("Playlist: http://%s:%d/playlist.m3u\n", me.hostname, me.port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", me.port), nil))
+}
+
+func (fs Files) ContainsPath(p string) bool {
+	for _, f := range fs {
+		if f.path == p {
+			return true
+		}
+	}
+	return false
 }
 
 func main() {
