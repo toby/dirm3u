f7fc5991e0061fe15f2214e50c6ebaffcc6f444d
Toby Padilla <tobypadilla@gmail.com>
2019-01-26 13:40:08 -0600

    Initial html video list

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
@@ -11,38 +12,45 @@ import (
 )
 
 type File struct {
-	info os.FileInfo
-	path string
+	Info os.FileInfo
+	Path string
 }
 
 type Files []File
 
 type Server struct {
-	port     int
-	hostname string
-	files    Files
+	Port          int
+	Hostname      string
+	Files         Files
+	IndexTemplate *template.Template
 }
 
 func NewServer(p int, h string) Server {
-	return Server{
-		port:     p,
-		hostname: h,
-		files:    make([]File, 0),
+	tmpl, err := template.New("index.tmpl").ParseFiles("index.tmpl")
+	if err != nil {
+		panic(err)
 	}
+	s := Server{
+		Port:          p,
+		Hostname:      h,
+		Files:         make([]File, 0),
+		IndexTemplate: tmpl,
+	}
+	return s
 }
 
 func (me *Server) loadFiles() {
-	me.files = make([]File, 0)
+	me.Files = make([]File, 0)
 	err := filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
 		if SupportedType(path) {
 			fmt.Printf("Adding file: %s\n", path)
-			me.files = append(me.files, File{info, path})
+			me.Files = append(me.Files, File{info, path})
 		} else {
 			fmt.Printf("Skipping file: %s\n", path)
 		}
 		return nil
 	})
-	sort.Sort(me.files)
+	sort.Sort(me.Files)
 	if err != nil {
 		panic(err)
 	}
@@ -50,15 +58,22 @@ func (me *Server) loadFiles() {
 
 func (me *Server) m3uHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "application/mpegurl")
-	for _, f := range me.files {
-		fmt.Fprintf(w, "http://%s:%d/media/%s\n", me.hostname, me.port, f.path)
+	for _, f := range me.Files {
+		fmt.Fprintf(w, "http://%s:%d/media/%s\n", me.Hostname, me.Port, f.Path)
+	}
+}
+
+func (me *Server) indexHandler(w http.ResponseWriter, r *http.Request) {
+	err := me.IndexTemplate.Execute(w, me)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
 	}
 }
 
 func (me *Server) mediaHandler(w http.ResponseWriter, r *http.Request) {
 	// trim `/media/`
 	f := r.URL.Path[7:]
-	if me.files.ContainsPath(f) {
+	if me.Files.ContainsPath(f) {
 		http.ServeFile(w, r, f)
 	} else {
 		http.NotFound(w, r)
@@ -67,24 +82,25 @@ func (me *Server) mediaHandler(w http.ResponseWriter, r *http.Request) {
 
 func (me *Server) reloadHandler(w http.ResponseWriter, r *http.Request) {
 	me.loadFiles()
-	fmt.Fprintf(w, "Reloaded %d files\n", len(me.files))
-	for _, f := range me.files {
-		fmt.Fprintf(w, "http://%s:%d/media/%s\n", me.hostname, me.port, f.path)
+	fmt.Fprintf(w, "Reloaded %d files\n", len(me.Files))
+	for _, f := range me.Files {
+		fmt.Fprintf(w, "http://%s:%d/media/%s\n", me.Hostname, me.Port, f.Path)
 	}
 }
 
 func (me *Server) Start() {
 	me.loadFiles()
+	http.HandleFunc("/", me.indexHandler)
 	http.HandleFunc("/playlist.m3u", me.m3uHandler)
 	http.HandleFunc("/reload", me.reloadHandler)
 	http.HandleFunc("/media/", me.mediaHandler)
-	fmt.Printf("Playlist: http://%s:%d/playlist.m3u\n", me.hostname, me.port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", me.port), nil))
+	fmt.Printf("Playlist: http://%s:%d/playlist.m3u\n", me.Hostname, me.Port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", me.Port), nil))
 }
 
 func (fs Files) ContainsPath(p string) bool {
 	for _, f := range fs {
-		if f.path == p {
+		if f.Path == p {
 			return true
 		}
 	}
@@ -96,7 +112,7 @@ func (fs Files) Len() int {
 }
 
 func (fs Files) Less(i int, j int) bool {
-	return fs[i].info.ModTime().Before(fs[j].info.ModTime())
+	return fs[i].Info.ModTime().After(fs[j].Info.ModTime())
 }
 
 func (fs Files) Swap(i int, j int) {
