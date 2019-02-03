7090438967cb5ae7de4f9686b6ef24a9830b80b0
Toby Padilla <tobypadilla@gmail.com>
2019-02-03 17:54:29 -0600

    File tags for web and vlc

--- extensions.go
+++ extensions.go
@@ -1,84 +1,91 @@
 package main
 
 import (
+	"fmt"
 	"path/filepath"
 	"strings"
 )
 
 var webExtensions = map[string]bool{
-	".WEBM": true,
-	".MP4":  true,
-	".OGG":  true,
+	".webm": true,
+	".mp4":  true,
+	".ogg":  true,
 }
 
 var vlcExtensions = map[string]bool{
-	".ASX":   true,
-	".DTS":   true,
-	".GXF":   true,
-	".M2V":   true,
-	".M3U":   true,
-	".M4V":   true,
-	".MPEG1": true,
-	".MPEG2": true,
-	".MTS":   true,
-	".MXF":   true,
-	".OGM":   true,
-	".PLS":   true,
-	".BUP":   true,
-	".A52":   true,
-	".AAC":   true,
-	".B4S":   true,
-	".CUE":   true,
-	".DIVX":  true,
-	".DV":    true,
-	".FLV":   true,
-	".M1V":   true,
-	".M2TS":  true,
-	".MKV":   true,
-	".MOV":   true,
-	".MPEG4": true,
-	".OMA":   true,
-	".SPX":   true,
-	".TS":    true,
-	".VLC":   true,
-	".VOB":   true,
-	".XSPF":  true,
-	".DAT":   true,
-	".BIN":   true,
-	".IFO":   true,
-	".PART":  true,
-	".3G2":   true,
-	".AVI":   true,
-	".MPEG":  true,
-	".MPG":   true,
-	".FLAC":  true,
-	".M4A":   true,
-	".MP1":   true,
-	".OGG":   true,
-	".WAV":   true,
-	".XM":    true,
-	".3GP":   true,
-	".SRT":   true,
-	".WMV":   true,
-	".AC3":   true,
-	".ASF":   true,
-	".MOD":   true,
-	".MP2":   true,
-	".MP3":   true,
-	".MP4":   true,
-	".WMA":   true,
-	".MKA":   true,
-	".M4P":   true,
-	".WEBM":  true,
+	".asx":   true,
+	".dts":   true,
+	".gxf":   true,
+	".m2v":   true,
+	".m3u":   true,
+	".m4v":   true,
+	".mpeg1": true,
+	".mpeg2": true,
+	".mts":   true,
+	".mxf":   true,
+	".ogm":   true,
+	".pls":   true,
+	".bup":   true,
+	".a52":   true,
+	".aac":   true,
+	".b4s":   true,
+	".cue":   true,
+	".divx":  true,
+	".dv":    true,
+	".flv":   true,
+	".m1v":   true,
+	".m2ts":  true,
+	".mkv":   true,
+	".mov":   true,
+	".mpeg4": true,
+	".oma":   true,
+	".spx":   true,
+	".ts":    true,
+	".vlc":   true,
+	".vob":   true,
+	".xspf":  true,
+	".dat":   true,
+	".bin":   true,
+	".ifo":   true,
+	".part":  true,
+	".3g2":   true,
+	".avi":   true,
+	".mpeg":  true,
+	".mpg":   true,
+	".flac":  true,
+	".m4a":   true,
+	".mp1":   true,
+	".ogg":   true,
+	".wav":   true,
+	".xm":    true,
+	".3gp":   true,
+	// ".srt":   true,
+	".wmv":  true,
+	".ac3":  true,
+	".asf":  true,
+	".mod":  true,
+	".mp2":  true,
+	".mp3":  true,
+	".mp4":  true,
+	".wma":  true,
+	".mka":  true,
+	".m4p":  true,
+	".webm": true,
 }
 
-func SupportedType(t string, p string) bool {
-	var ok bool
-	ext := strings.ToUpper(filepath.Ext(p))
-	if t == "web" {
-		_, ok = webExtensions[ext]
-	} else {
-		_, ok = vlcExtensions[ext]
+func FileTags(p string) ([]string, error) {
+	ts := make([]string, 0)
+	ext := strings.ToLower(filepath.Ext(p))
+	_, ok := webExtensions[ext]
+	if ok {
+		ts = append(ts, "web")
 	}
-	return ok
+	_, ok = vlcExtensions[ext]
+	if ok {
+		ts = append(ts, "vlc")
+	}
+	if len(ts) == 0 {
+		return ts, fmt.Errorf("file type %s not supported", ext)
+	}
+	return ts, nil
 }
--- main.go
+++ main.go
@@ -27,7 +27,7 @@ type Files []*File
 
 type FileDB struct {
 	files Files
-	tags  map[string][]*File
+	tags  map[string]Files
 }
 
 type Server struct {
@@ -45,8 +45,7 @@ type Page struct {
 
 func NewFileDB() FileDB {
 	return FileDB{
-		files: make([]*File, 0),
-		tags:  make(map[string][]*File),
+		tags: make(map[string]Files),
 	}
 }
 
@@ -54,18 +53,34 @@ func (db *FileDB) Files() Files {
 	return db.files
 }
 
+func (db *FileDB) ForTag(t string) (Files, bool) {
+	fs, err := db.tags[t]
+	return fs, err
+}
+
 func (db *FileDB) loadFiles() {
 	db.files = make([]*File, 0)
 	err := filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
-		if SupportedType("web", path) {
-			fmt.Printf("Adding file: %s\n", path)
-			db.files = append(db.files, &File{info, path})
-		} else {
+		ts, err := FileTags(path)
+		if err != nil {
 			fmt.Printf("Skipping file: %s\n", path)
+		} else {
+			fmt.Printf("Adding file: %s\n", path)
+			f := &File{info, path}
+			db.files = append(db.files, f)
+			for _, t := range ts {
+				db.tags[t] = append(db.tags[t], f)
+			}
 		}
 		return nil
 	})
 	sort.Sort(db.files)
+	for k, _ := range db.tags {
+		fs, _ := db.tags[k]
+		sort.Sort(fs)
+		db.tags[k] = fs
+		fmt.Printf("File Tag: '%s' has %d files\n", k, len(db.tags[k]))
+	}
 	if err != nil {
 		panic(err)
 	}
@@ -96,7 +111,12 @@ func NewServer(p int, h string, l int) Server {
 
 func (me *Server) m3uHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	w.Header().Add("Content-Type", "application/mpegurl")
-	for _, f := range me.db.Files() {
+	fs, ok := me.db.ForTag("vlc")
+	if !ok {
+		http.Error(w, "Page not found", 404)
+		return
+	}
+	for _, f := range fs {
 		fmt.Fprintf(w, "http://%s/media/%s\n", me.HostPort(), f.Path)
 	}
 }
@@ -112,15 +132,18 @@ func (me *Server) indexHandler(w http.ResponseWriter, r *http.Request, ps httpro
 		i, err = strconv.Atoi(n)
 		if err != nil {
 			http.Error(w, err.Error(), 500)
+			return
 		}
 	}
 	if i > len(me.Pages()) {
 		http.Error(w, "Page not found", 404)
+		return
 	}
 	p.Index = i - 1
 	err = me.indexTemplate.Execute(w, p)
 	if err != nil {
 		http.Error(w, err.Error(), 500)
+		return
 	}
 }
 
@@ -164,13 +187,21 @@ func (me *Server) HostPort() string {
 }
 
 func (me *Server) PageNums() int64 {
-	return int64(math.Ceil(float64(len(me.db.Files())) / float64(me.Limit)))
+	fs, ok := me.db.ForTag("web")
+	if !ok {
+		return 0
+	}
+	return int64(math.Ceil(float64(len(fs)) / float64(me.Limit)))
 }
 
 func (me *Server) Pages() [][]*File {
 	var p []*File
 	ps := make([][]*File, 0)
-	for n, f := range me.db.Files() {
+	fs, ok := me.db.ForTag("web")
+	if !ok {
+		return ps
+	}
+	for n, f := range fs {
 		if n%me.Limit == 0 {
 			if n > 0 {
 				ps = append(ps, p)
